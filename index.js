492ff8c449aedffa1fe86bddbb24cf39c6ee87ee
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-01-17 17:23:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -89,10 +89,16 @@ Car.prototype.fill = function (gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+Baby.prototype = Object.create(Person.prototype);
+function Baby(name, age, favoriteToy) {
+  Person.call(this, favoriteToy);
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype.play = function() {
+  return "Playing with " + this.favoriteToy;
+};
 /* 
   TASK 4
 
